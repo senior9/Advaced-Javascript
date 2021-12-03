af745dc5e2a03042f704b65261e67adb94d8c089
senior9 <abhishek215188@gmail.com>
2021-12-03 16:16:28 +0600

    True Vs False

---
+++ true-false.js
@@ -0,0 +1,25 @@
+// Falsy Item :
+// 0
+// ""
+// ''
+// null
+// 
+
+
+//  Truthy
+// {}
+// []
+// Number
+// white space
+// string
+// array
+
+const name = '';
+
+if (name) {
+    console.log("condition is true");
+}
+else{
+    console.log("condition is False");
+
+}
