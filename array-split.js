b2d40d294e36251ce04a0abf610a08e4c2858608
senior9 <abhishek215188@gmail.com>
2021-12-05 19:46:53 +0600

    array-split

---
+++ array-split.js
@@ -0,0 +1,15 @@
+const names = ["Abhishek", "Aditi", "Adai", "Bhodai", "Alu","Tablet"];
+// const sliceNames = names.slice(3, 5);
+const spliceNames =names.splice(2, 2, "Abhishek Sikdar","Mukul SIkdar");
+console.log(spliceNames);
+
+const joinNames = names.join("");
+console.log(joinNames);
+const number = [ 12, 15, 14, 85, 15, 12];
+const joinNumber = number.join(" ,");
+console.log(joinNumber);
+console.log(number);
+
+
+// console.log(sliceNames);
+console.log(names);
